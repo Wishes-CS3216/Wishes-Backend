070bfe70f4e80df5da34decc1bd591a38ac4d39f
Bohan Huang <weepineapple@gmail.com>
2016-09-14 15:11:55 +0800

    Updated commented routes

--- Wishes/config/routes.rb
+++ Wishes/config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=begin
+	scope '/api' do
+		scope '/v1' do
+			scope '/users' do
+				post '/login' => 'users#login'
+				post '/signup' => 'users#create'
+
+				scope '/:id' do
+					get '/' => 'user#show'
+
+					scope '/reports' do
+						post '/' => 'reports#create'
+					end
+
+					scope '/activities' do
+						get '/' => 'activities#index'
+						post '/archive' => 'activities#archive'
+					end
+
+					scope '/wishes' do
+						get '/' => 'wishes#index'
+						post '/' => 'wishes#create' 
+						scope '/:id' do
+							get '/' => 'wishes#show'
+							get '/edit' => 'wishes#edit'
+							put '' => 'wishes#update'
+						end
+					end
+				end
+			
+			end
+		end
+	end
+=end
 end
