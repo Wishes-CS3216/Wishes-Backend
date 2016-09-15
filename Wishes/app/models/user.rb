004ffd64b3d47bbc2f28f16190e4bed264aad639
Melvin <melvin0719@hotmail.com>
2016-09-16 03:09:19 +0800

    Validates password presence

--- Wishes/app/models/user.rb
+++ Wishes/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many :wishes
 
 	validates :username, presence: true, uniqueness: true
+	validates :password, presence: true
 
 	scope :get_user, -> (user_id, username, password) { where id: user_id, username: username, password: password }
 end
