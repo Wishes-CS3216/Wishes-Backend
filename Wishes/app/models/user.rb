3247f6f1b0ecbb75081a51a87ade5cd205df2c3d
Melvin <melvin0719@hotmail.com>
2016-09-16 02:55:24 +0800

    Add validate uniqueness of username

--- Wishes/app/models/user.rb
+++ Wishes/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :wishes
 
+	validates :username, uniqueness: true
+
 	scope :get_user, -> (user_id, username, password) { where id: user_id, username: username, password: password }
 end
