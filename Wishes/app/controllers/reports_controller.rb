ed058ee7d56edba816f71f9493ac833889ffe6f2
Melvin <melvin0719@hotmail.com>
2016-09-17 09:29:30 +0800

    Resolve report foreign keys

--- Wishes/app/controllers/reports_controller.rb
+++ Wishes/app/controllers/reports_controller.rb
@@ -3,10 +3,11 @@ class ReportsController < ApplicationController
 		@report = Report.new(report_params)
 		Report.transaction do
 			@report.save!
+			render json: {status: "Created report successfully"}
 		end
 	end
 private
 	def report_params
-		params.permit([:reporter_id, :reportee_id])
+		params.permit([:user_id, :reported_user_id])
 	end
 end
--- Wishes/app/models/user.rb
+++ Wishes/app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
 	has_many :wishes
 	has_many :reports
 	has_many :activities
