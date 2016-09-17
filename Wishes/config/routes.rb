d84ea455c816e92fc96156c16274bdc8544c7970
Melvin <melvin0719@hotmail.com>
2016-09-17 22:07:08 +0800

    Change post back to put for user update

--- Wishes/config/routes.rb
+++ Wishes/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
 				scope '/:user_id' do
 					get '/' => 'users#show'
-					post '/' => 'users#update'
+					put '/' => 'users#update'
 
 					scope '/reports' do
 						post '/' => 'reports#create'
