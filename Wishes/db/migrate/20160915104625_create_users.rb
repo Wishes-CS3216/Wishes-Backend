396a462d1e0990484f3fb958aa60dd6bc51cdd5d
Bohan Huang <weepineapple@gmail.com>
2016-09-19 12:21:53 +0800

    Updated schema to remove password column from users

--- Wishes/db/migrate/20160915104625_create_users.rb
+++ Wishes/db/migrate/20160915104625_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
       t.string :phone
       t.string :email
       t.boolean :email_verified
